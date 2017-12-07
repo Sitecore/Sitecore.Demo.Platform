c9dd7360bac5b9a1106e0fa3a529f1689aaf818f
J.F. Larente <jfl@sitecore.net>
2017-12-07 10:32:42 -0500

    Modifying scripts to get latest .json assets
    Former-commit-id: 71366ca738cd24a967528beb086397a07f7bd73b

--- install/get-latest-sitecore.ps1
+++ install/get-latest-sitecore.ps1
@@ -1,22 +1,21 @@
-$rootQA = "\\mars\qa\9.0\9.0.1\nightly"
-$xpJsonFileName = "wdpUrls_CMS_OnPrem.json"
-$xConnectJsonFileName = "wdpUrls_xConnect_OnPrem.json"
+$rootQA = "\\mars\qa\9.0\9.0.1"
+$jsonFileName = (Get-ChildItem "$rootQA" -File -Recurse | ?{ $_.Name -eq "wdpUrls_OnPrem.json" } | Sort-Object LastWriteTime -Descending | Select-Object -First 1).FullName
+$WdpResourcesFeed = "http://nuget1dk1/nuget/9.0.1_master/"
 $downloadFolder = ".\assets"
 $sxaPackageUrl = "http://tcbuild-ext1ua1:8080/guestAuth/repository/download/Sxa90Build_Nightly/.lastSuccessful/Sitecore%20Experience%20Accelerator%20{build.number}.zip"
 $spePackageUrl = "https://marketplace.sitecore.net/services/~/download/BA9304F736324923A4D034FF4D8D4F2D.ashx?data=Sitecore%20PowerShell%20Extensions-4.7%20for%20Sitecore%208&itemId=6aaea046-83af-4ef1-ab91-87f5f9c1aa57"
-$currentVersionFolder = $(Get-ChildItem $rootQA -Filter "Sitecore*" | Sort LastWriteTime | Select -Last 1)
 
-Function GetJson($path){
 
-}
+$json = $(Get-Content $jsonFileName -Raw | ConvertFrom-Json)
 
-$xpUrlJson = $(Get-Content $(Join-Path $currentVersionFolder.Fullname $xpJsonFileName) -Raw | ConvertFrom-Json)
+$sitecorePackageUrl = $json.xp0.single
+$xConnectPackageUrl = $json.xp0.xconnect
+$resources = $json.resources
+$resourcesName = "Sitecore.WDP.Resources"
+$resourcesVersion = $resources.Replace($resourcesName + ".","")
 
-$sitecorePackageUrl = $xpUrlJson.xp0.single
-
-$xConnectUrlJson = $(Get-Content $(Join-Path $currentVersionFolder.Fullname $xConnectJsonFileName) -Raw | ConvertFrom-Json)
-
-$xConnectPackageUrl = $xConnectUrlJson.xp0.xconnect
+Write-Host "Installing Resource Version $resourcesVersion" -ForegroundColor Green
+nuget install $resourcesName -Version $resourcesVersion -Source $WdpResourcesFeed -OutputDirectory $downloadFolder -x -prerelease
 
 $sitecorePackagePaths = $sitecorePackageUrl.Split("?")
 $sitecorePackageFileName = $sitecorePackagePaths[0].substring($sitecorePackagePaths[0].LastIndexOf("/") + 1)
@@ -27,12 +26,16 @@ $xConnectPackageFileName = $xConnectPackagePaths[0].substring($xConnectPackagePa
 $sitecoreDestination = $([io.path]::combine($downloadFolder, $sitecorePackageFileName)).ToString()
 $xConnectDestination = $([io.path]::combine($downloadFolder, $xConnectPackageFileName)).ToString()
 
+Write-Host "Saving $sitecorePackageUrl to $sitecoreDestination - if required" -ForegroundColor Green
 if (!(Test-Path $sitecoreDestination)) {
     Start-BitsTransfer -Source $sitecorePackageUrl -Destination $sitecoreDestination
 }
+
+Write-Host "Saving $xConnectPackageUrl to $xConnectDestination - if required" -ForegroundColor Green
 if (!(Test-Path $xConnectDestination)) {
     Start-BitsTransfer -Source $xConnectPackageUrl -Destination $xConnectDestination
 }
 New-Item -ItemType Directory -Force -Path $($downloadFolder + "\packages")
+Write-Host "Downloading latest SPE and SXA" -ForegroundColor Green
 Start-BitsTransfer -Source $sxaPackageUrl -Destination $([io.path]::combine($downloadFolder, "packages\sxa-nightly.zip")) 
 Start-BitsTransfer -Source $spePackageUrl -Destination $([io.path]::combine($downloadFolder, "packages\spe-latest.zip"))
