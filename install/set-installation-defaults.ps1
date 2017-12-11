2cb435912fca2a3974de56a8c145c87a2e591c59
Jean-François Larente <jfl@sitecore.net>
2017-12-11 11:21:51 -0500

    Moving Resources to shallower /install path instead of install/assets to to path too long error Modified set-installation-defaults accordingly Updated SXA and .json files
    Former-commit-id: 9768d087bf785c4de405b2835782f196480e1b5d

--- install/set-installation-defaults.ps1
+++ install/set-installation-defaults.ps1
@@ -40,9 +40,9 @@ $sql.minimumVersion="13.0.4001"
 # XConnect Parameters
 $xConnect = $json.settings.xConnect
 
-$xConnect.ConfigurationPath = (Get-ChildItem $assets.root -filter "xconnect-xp0.json" -Recurse).FullName 
+$xConnect.ConfigurationPath = (Get-ChildItem $pwd -filter "xconnect-xp0.json" -Recurse).FullName 
 $xConnect.certificateConfigurationPath = (Get-ChildItem $assets.root -filter "xconnect-createcert.json" -Recurse).FullName
-$xConnect.solrConfigurationPath = (Get-ChildItem $assets.root -filter "xconnect-solr.json" -Recurse).FullName 
+$xConnect.solrConfigurationPath = (Get-ChildItem $pwd -filter "xconnect-solr.json" -Recurse).FullName 
 $xConnect.packagePath = Join-Path $assets.root $("Sitecore " + $assets.sitecoreVersion + " (OnPrem)_xp0xconnect.scwdp.zip")
 $xConnect.siteName = $site.prefix + "_xconnect." + $site.suffix
 $xConnect.certificateName = [string]::Join(".", @($site.prefix, $site.suffix, "xConnect.Client"))
@@ -54,8 +54,8 @@ $xConnect.sqlCollectionPassword = "Test12345"
 # Sitecore Parameters
 $sitecore = $json.settings.sitecore
 
-$sitecore.solrConfigurationPath =  (Get-ChildItem $assets.root -filter "sitecore-solr.json" -Recurse).FullName 
-$sitecore.configurationPath = (Get-ChildItem $assets.root -filter "sitecore-xp0.json" -Recurse).FullName 
+$sitecore.solrConfigurationPath =  (Get-ChildItem $pwd -filter "sitecore-solr.json" -Recurse).FullName 
+$sitecore.configurationPath = (Get-ChildItem $pwd -filter "sitecore-xp0.json" -Recurse).FullName 
 $sitecore.sslConfigurationPath = "$PSScriptRoot\certificates\sitecore-ssl.json"
 $sitecore.packagePath = Join-Path $assets.root $("Sitecore " + $assets.sitecoreVersion +" (OnPrem)_single.scwdp.zip")
 $sitecore.siteName = [string]::Join(".", @($site.prefix, $site.suffix))
