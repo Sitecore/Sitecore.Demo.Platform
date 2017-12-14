0d75e4b9e46228cb05785a972cb8184d6e9e9f41
Elena Mosoff <emosoff@hhogdev.com>
2017-12-14 11:45:37 -0500

    added forms projects to solution
    Former-commit-id: 4448375d42ab93c6197faa1a7f65ea3532565b47

---
+++ src/Feature/Forms/code/SubmitActions/AssignPatternCardAction.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.ExperienceForms.Processing.Actions;
+using Sitecore.ExperienceForms.Processing.Actions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.Diagnostics;
+using Sitecore.Data;
+
+namespace Sitecore.Feature.Forms.SubmitActions
+{
+    public class AssignPatternCardAction : SubmitActionBase<RedirectActionData>
+    {
+        protected AssignPatternCardAction(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+        }
+
+        protected override bool Execute(RedirectActionData data, FormSubmitContext formSubmitContext)
+        {
+            Assert.ArgumentNotNull(formSubmitContext, "formSubmitContext");
+            if (data == null || !(data.ReferenceId != Guid.Empty))
+                return false;
+            var item = Sitecore.Context.Database.GetItem(new ID(data.ReferenceId));
+            if (item == null)
+                return false;
+
+            var email = string.Empty;
+            var field = formSubmitContext.Fields.FirstOrDefault(f => f.Name.Equals("Email"));
+            if (field != null)
+            {
+                var property = field.GetType().GetProperty("Value");
+                var postedEmail = property.GetValue(field);          
+            }
+                                                 
+            return true;
+        }
+    }
+}
