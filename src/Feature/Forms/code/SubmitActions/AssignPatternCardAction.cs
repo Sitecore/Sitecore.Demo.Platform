1e4821371dd6a5c2403efb6f18f584fe03340c19
Elena Mosoff <emosoff@hhogdev.com>
2017-12-15 13:39:57 -0500

    assign patterns submit action with configuration templates and action editor
    Former-commit-id: ebe460552846dd5848b5329e3df874e2ab943473

--- src/Feature/Forms/code/SubmitActions/AssignPatternCardAction.cs
+++ src/Feature/Forms/code/SubmitActions/AssignPatternCardAction.cs
@@ -1,40 +1,142 @@
-﻿using Sitecore.ExperienceForms.Processing.Actions;
-using Sitecore.ExperienceForms.Processing.Actions.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using Sitecore.ExperienceForms.Processing;
 using Sitecore.ExperienceForms.Models;
 using Sitecore.Diagnostics;
 using Sitecore.Data;
+using Sitecore.Data.Items;
+using System.Xml;
+using System.Collections;
+using Sitecore.ExperienceForms.SubmitActions;
+using Sitecore.Analytics;
+using Sitecore.Analytics.Tracking;
+using Sitecore.Foundation.SitecoreExtensions.Extensions;
 
 namespace Sitecore.Feature.Forms.SubmitActions
 {
-    public class AssignPatternCardAction : SubmitActionBase<RedirectActionData>
+    public class AssignPatternCardAction : AnalyticsActionBase<AssignPatternActionData>
     {
-        protected AssignPatternCardAction(ISubmitActionData submitActionData) : base(submitActionData)
+        public AssignPatternCardAction(ISubmitActionData submitActionData) : base(submitActionData)
         {
         }
 
-        protected override bool Execute(RedirectActionData data, FormSubmitContext formSubmitContext)
+        protected override bool Execute(AssignPatternActionData data, FormSubmitContext formSubmitContext)
         {
             Assert.ArgumentNotNull(formSubmitContext, "formSubmitContext");
             if (data == null || !(data.ReferenceId != Guid.Empty))
-                return false;
-            var item = Sitecore.Context.Database.GetItem(new ID(data.ReferenceId));
-            if (item == null)
-                return false;
-
-            var email = string.Empty;
-            var field = formSubmitContext.Fields.FirstOrDefault(f => f.Name.Equals("Email"));
-            if (field != null)
             {
-                var property = field.GetType().GetProperty("Value");
-                var postedEmail = property.GetValue(field);          
+                // submit action was not configured properly     
+                Log.Error(string.Format("AssignPatternCardAction failed: Submit action settings were not configured properly for form {0}.", formSubmitContext.FormId), this);
+                return false;
             }
-                                                 
+
+            Item item = Context.Database.GetItem(new ID(data.ReferenceId));
+            if (item == null || !item.IsDerived(Templates.PatternCardActionSettings.ID))
+            {
+                // submit action was not configured properly    
+                Log.Error(string.Format("AssignPatternCardAction failed: Submit action settings for form {0} point to an invalid item.", formSubmitContext.FormId), this);
+                return false;
+            }
+
+            string questionId = item[Templates.PatternCardActionSettings.Fields.FormQuestion];
+            if (questionId == null)
+            {
+                // settings item was not configured properly
+                Log.Error(string.Format("AssignPatternCardAction failed: Submit action settings were not configured properly. Form Question on settings item {0} is not assigned or does not exist in this context.", data.ReferenceId), this);
+                return false;
+            }
+
+            questionId = questionId.Replace("{", string.Empty).Replace("}", string.Empty).ToLower();
+            IViewModel fieldModel = formSubmitContext.Fields.FirstOrDefault(f => f.ItemId == questionId);
+            if (fieldModel == null)
+            {
+                // no submitted field matched the configured form question          
+                Log.Error(string.Format("AssignPatternCardAction failed: Configured question ID {0} does not exist form {1}", questionId, formSubmitContext.FormId), this);
+                return false;
+            }
+
+            string answer = string.Empty;
+            try
+            {
+                var property = fieldModel.GetType().GetProperty("Value");
+                var postedValue = property.GetValue(fieldModel);
+                answer = ParseFieldValue(postedValue);
+            }
+            catch (Exception ex)
+            {
+                // could not get value of submitted field
+                Log.Error("AssignPatternCardAction failed: could not parse value of submitted field.", ex, this);
+                return false;
+            }
+
+            var mappings = item.GetChildren();
+            var mapping = mappings.FirstOrDefault(m => m[Templates.PatternCardActionMapping.Fields.MappedValue].ToLower().Trim() == answer.ToLower().Trim());
+            if (mapping == null)
+            {
+                // could not find a mapping setting item for the submitted value
+                // log warning and continue execution without failure
+                Log.Warn(string.Format("AssignPatternCardAction failed: No mapping found for answer {0} of question {1}", answer, questionId), this);
+                return true;
+            }
+
+            string patternCardId = mapping["Pattern Card"];
+            Item matchedPatternCard = Context.Database.GetItem(patternCardId);
+
+            try
+            {
+                if (Tracker.Current == null)
+                {
+                    Tracker.StartTracking();
+                }
+               
+                Profile profile = Tracker.Current.Interaction.Profiles[matchedPatternCard.Parent.Parent.Name];
+
+                Data.Fields.XmlField xmlData = matchedPatternCard.Fields["Pattern"];
+                XmlDocument xmlDoc = xmlData.Xml;
+
+                XmlNodeList parentNode = xmlDoc.GetElementsByTagName("key");
+                Dictionary<string, double> scores = new Dictionary<string, double>();
+
+                foreach (XmlNode childrenNode in parentNode)
+                {
+                    if (childrenNode.Attributes["value"].Value != "0")
+                    {
+                        double keyValue;
+                        double.TryParse(childrenNode.Attributes["value"].Value, out keyValue);
+                        scores.Add(childrenNode.Attributes["name"].Value, keyValue * 3);
+                    }
+                }
+
+                profile.Score(scores);
+                profile.UpdatePattern();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(string.Format("AssignPatternCardAction failed: Could not update pattern for card {0}", patternCardId), ex, this);
+                return false;
+            }                 
+
             return true;
+        }
+        protected static string ParseFieldValue(object postedValue)
+        {
+            Assert.ArgumentNotNull(postedValue, nameof(postedValue));
+            List<string> stringList = new List<string>();
+            IList list = postedValue as IList;
+            if (list != null)
+            {
+                foreach (object obj in list)
+                {
+                    stringList.Add(obj.ToString());
+                }
+            }
+            else
+            {
+                stringList.Add(postedValue.ToString());
+            }
+
+            return string.Join(",", stringList);
         }
     }
 }
---
+++ src/Feature/Forms/code/Templates.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Data;     
+
+namespace Sitecore.Feature.Forms
+{
+    public struct Templates
+    {
+        public struct PatternCardActionSettings
+        {
+            public static readonly ID ID = new ID("{C6D4DDD5-B912-4C1A-A3A3-E1D90E4D0939}");
+
+            public struct Fields
+            {
+                public static readonly ID FormQuestion = new ID("{8C512534-BA02-49FC-91BE-66DA6E6AA1B6}");
+            }
+        }
+
+        public struct PatternCardActionMapping
+        {
+            public static readonly ID ID = new ID("{4FDED342-B304-42F7-8091-AE3EB660BA4C}");
+
+            public struct Fields
+            {
+                public static readonly ID PatternCard = new ID("{E30632B3-B1C5-494E-8EB2-5C323FE80C5E}");
+                public static readonly ID MappedValue = new ID("{ECB328C3-8F41-4C0B-8FDF-A01096146F3B}");
+            }
+        }
+    }
+}
