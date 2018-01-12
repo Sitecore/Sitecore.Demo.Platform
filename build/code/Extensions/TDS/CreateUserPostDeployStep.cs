4dd47b60ac6408744b44a4ea34edfc657e59a689
Svetlana <spareva@abv.bg>
2018-01-12 14:21:00 +0200

    admin user, updated workflow
    Related Work Items: #520, #523
    
    
    Former-commit-id: a937396adf6d03e93bcab7411c031295f76b31da

---
+++ build/code/Extensions/TDS/CreateUserPostDeployStep.cs
@@ -0,0 +1,25 @@
+﻿using HedgehogDevelopment.SitecoreProject.PackageInstallPostProcessor.Contracts;
+using Sitecore.Security.Accounts;
+using System.Xml.Linq;
+
+namespace Sitecore.Demo.Deployment.Web.Extensions.TDS
+{
+    public class CreateUserPostDeployStep : IPostDeployAction
+    {
+        public void RunPostDeployAction(XDocument deployedItems, IPostDeployActionHost host, string parameter)
+        {
+            string demoAdminUsername = "sitecore/demoadmin";
+            host.LogMessage("demoadminusername: " + demoAdminUsername);
+
+            string demoAdminDomainUsername = Security.Domains.Domain.GetDomain("sitecore").GetFullName("demoadmin");
+            host.LogMessage("demoadmindomainusername: " + demoAdminDomainUsername);
+
+            if (!User.Exists(demoAdminUsername))
+            {
+                var user = User.Create(demoAdminUsername, "demopass");
+                user.Profile.Email = "support@sitecore.net";
+                user.Profile.Save();
+            }
+        }
+    }
+}
