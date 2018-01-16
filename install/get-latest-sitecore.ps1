8dfd144b09c2eec3fec9f8ad8016c86278af3977
Jean-François Larente <jfl@sitecore.net>
2018-01-16 09:48:23 -0500

    Modifying Get-Latest-Sitecore to use full file names

--- install/get-latest-sitecore.ps1
+++ install/get-latest-sitecore.ps1
@@ -5,8 +5,8 @@ $rootQA = "\\fil1ca2\mars\QA\9.0\9.0.1"
 
 $jsonLocalFileName = ".\assets\wdpUrls_OnPrem.json"
 $WdpResourcesFeed = "http://nuget1dk1/nuget/9.0.1_master/"
-$downloadFolder = ".\assets"
-$sxaPackageUrl = "http://tcbuild-ext1ua1:8080/guestAuth/repository/download/Sxa90Build_Nightly/.lastSuccessful/Sitecore%20Experience%20Accelerator%20{build.number}.zip"
+$downloadFolder = ".\assets\"
+$sxaPackageUrl = "https://v9assets.blob.core.windows.net/v9-onprem-assets/Sitecore%20Experience%20Accelerator%201.6%20rev.%20180103%20for%209.0.zip"
 $spePackageUrl = "https://marketplace.sitecore.net/services/~/download/BA9304F736324923A4D034FF4D8D4F2D.ashx?data=Sitecore%20PowerShell%20Extensions-4.7%20for%20Sitecore%208&itemId=6aaea046-83af-4ef1-ab91-87f5f9c1aa57"
 
 
@@ -68,6 +68,6 @@ New-Item -ItemType Directory -Force -Path $($downloadFolder + "\packages")
 if ($useLocal -eq $false) {
     Write-Host "Downloading latest SPE and SXA" -ForegroundColor Green
 
-    Start-BitsTransfer -Source $sxaPackageUrl -Destination $([io.path]::combine($downloadFolder, "packages\sxa-nightly.zip")) 
-    Start-BitsTransfer -Source $spePackageUrl -Destination $([io.path]::combine($downloadFolder, "packages\spe-latest.zip"))
+    Start-BitsTransfer -Source $sxaPackageUrl -Destination $([io.path]::combine($downloadFolder, "packages\Sitecore Experience Accelerator 1.6 rev. 180103 for 9.0.zip")) 
+    Start-BitsTransfer -Source $spePackageUrl -Destination $([io.path]::combine($downloadFolder, "packages\Sitecore PowerShell Extensions-4.7 for Sitecore 8.zip"))
 }
