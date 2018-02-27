850d1fff8a548ffc5bedc75b66d4828e7f9335b3
Elena Mosoff <emosoff@hhogdev.com>
2018-02-27 09:09:52 -0500

    extended the GeneralLink field type to include ability to insert relative links (support for links to generated catalog pages)
    Related Work Items: #794

---
+++ src/Foundation/SitecoreExtensions/code/Controls/ExtendedGeneralLink.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Specialized;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Applications.ContentEditor;
+using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+
+namespace Sitecore.Foundation.SitecoreExtensions.Controls
+{
+    public class ExtendedGeneralLink : Link
+    {
+        public ExtendedGeneralLink()
+        {
+            Class = "scContentControl";
+            Activation = true;
+        }
+
+        public override void HandleMessage(Message message)
+        {
+            Assert.ArgumentNotNull(message, "message");
+            base.HandleMessage(message);
+
+            if (message["id"] != ID)
+                return;
+
+            switch (message.Name)
+            {
+                case "contentlink:relativelink":
+                    {
+                        var url = new UrlString(UIUtil.GetUri("control:RelativePathForm"));
+                        Insert(url.ToString(), new NameValueCollection
+                    {
+                        {
+                            "height",
+                            "425"
+                        }
+                    });
+                        break;
+                    }
+                default:
+                    {
+                        return;
+                    }
+            }
+        }
+    }
+}
