26a682abad9c5fcb53a0f338bf84c475709fed24
J.F. Larente <jfl@sitecore.net>
2018-05-23 16:20:07 -0400

    Additional files added

---
+++ src/Project/HabitatHome/code/Controllers/EmptyContentController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Sitecore.HabitatHome.Website.Controllers
+{
+    public class EmptyContentController : Controller
+    {
+        public ActionResult Render()
+        {
+            return new EmptyResult();
+        }
+    }
+}
