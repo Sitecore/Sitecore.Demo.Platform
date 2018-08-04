95e13ca85b87bfc3e8fc6d8254972e8135c9bd32
kimcu-on-thenet <kim.cuhoang@gmail.com>
2018-08-04 22:56:40 +0700

    Fixed CE within Federated Authentication - Exporting fail => Object moved to instead of full Html - New PageRequestServiceOverride and PageRequestOverride processor has been introduced - A patch file will replace the original PageRequest processor from Sitecore

---
+++ src/Foundation/CreativeExchange/code/Services/PageRequestServiceOverride.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.XA.Feature.CreativeExchange.Enums;
+using Sitecore.XA.Feature.CreativeExchange.Extensions;
+using Sitecore.XA.Feature.CreativeExchange.Models.Export;
+using Sitecore.XA.Feature.CreativeExchange.Services.Export;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Sitecore.HabitatHome.Foundation.CreativeExchange.Services
+{
+    public class PageRequestServiceOverride : PageRequestService
+    {
+        public PageRequestServiceOverride(PageContext pageContext) : base(pageContext)
+        {
+        }
+
+        protected override CookieContainer GetCookieContainer(string host, HttpCookieCollection cookies)
+        {
+            var container = base.GetCookieContainer(host, cookies);
+
+            if (this.PageContext.ExportContext.ExportOptions.MarkupMode != MarkupMode.EndUserSite
+                && cookies.AllKeys.Any(x => x == ".AspNet.Cookies"))
+            {
+                cookies.TransferCookie(".AspNet.Cookies", host, container);
+            }
+
+            return container;
+        }
+    }
+}
