dc500fb8104a5b6a1764ce61eb5d666e90cc71ba
J.F. Larente <jfl@sitecore.net>
2018-08-15 16:02:47 -0400

    renamed admin user to maintain convention

--- scripts/SPE/set-hostname.ps1
+++ scripts/SPE/set-hostname.ps1
@@ -1,14 +1,14 @@
 param(
     [string]$instanceUrl = "habitathome.dev.local",
     [string]$hostname,
-    [string]$username,
-    [string]$password)
+    [string]$adminUsername,
+    [string]$adminPassword)
 $ErrorActionPreference = 'Stop'
 
 Import-Module SPE
 Write-Host ("Connecting to {0}" -f $instanceUrl)
 
-$session = New-ScriptSession -Username "$username" -Password "$password" -ConnectionUri $("https://" + $instanceUrl)
+$session = New-ScriptSession -Username "$adminUsername" -Password "$adminPassword" -ConnectionUri $("https://" + $instanceUrl)
 
 Invoke-RemoteScript -Session $session -ScriptBlock { 
     (Get-Item -Path "master:/content/Habitat Sites/Habitat Home/Settings/Site Grouping/Habitat Home").HostName = ("{0}" -f $using:hostname)
