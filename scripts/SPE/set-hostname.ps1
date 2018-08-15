325f8df0f33ec90f7d1deecd0e491f84bf6c6256
J.F. Larente <jfl@sitecore.net>
2018-08-15 10:49:24 -0400

    Added ability to pass in username/password to set-hostname script

--- scripts/SPE/set-hostname.ps1
+++ scripts/SPE/set-hostname.ps1
@@ -1,4 +1,7 @@
-param([string]$instanceUrl)
+param(
+    [string]$instanceUrl,
+    [string]$username,
+    [string]$password)
 $ErrorActionPreference = 'Stop'
 
 $ScriptPath = Split-Path $MyInvocation.MyCommand.Path
@@ -6,8 +9,8 @@ $ScriptPath = Split-Path $MyInvocation.MyCommand.Path
 # This is an example PowerShell script that will remotely execute a Unicorn sync using the new CHAP authentication system.
 
 Import-Module SPE
-Write-Host ("Connecting to {0}"-f $instanceUrl)
+Write-Host ("Connecting to {0}" -f $instanceUrl)
 
-$session = New-ScriptSession -Username "admin" -Password "b" -ConnectionUri $("https://" + $instanceUrl)
+$session = New-ScriptSession -Username $username -Password $password -ConnectionUri $("https://" + $instanceUrl)
 
 Invoke-RemoteScript -Session $session -ScriptBlock { Get-Item -Path "master:/content/Habitat Sites/Habitat Home/Settings/Site Grouping/Habitat Home" | ForEach-Object {$_.HostName = "$instanceUrl"}}
