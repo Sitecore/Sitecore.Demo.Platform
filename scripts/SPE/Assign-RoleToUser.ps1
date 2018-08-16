f6ecd31317293ff49d9acc81aefaf81b59ac736e
J.F. Larente <jfl@sitecore.net>
2018-08-16 06:56:27 -0400

    Added ability to pass in username/password to set-hostname script (#48)
    * Added ability to pass in username/password to set-hostname script
    
    * Removing the ability to call the SPE functions from gulp. These functions can be called directly from scripts/SPE if/when required
    
    * - Added SPE Remoting scripts to
      - Add new user
      - Assign role to user
      - Disable a user
    
    * Adding demogod user as remote SPE user
    
    * Adding desc attribute to allow correct patching
    
    * Fixed scripts
    
    * formatting
    
    * Added instanceUrl default value and a 'hostname' parameter to the set-hostname script
    
    * renamed admin user to maintain convention

---
+++ scripts/SPE/Assign-RoleToUser.ps1
@@ -0,0 +1,18 @@
+param(
+    [string]$instanceUrl = "habitathome.dev.local",
+    [string]$adminUsername,
+    [string]$adminPassword,
+    [string]$username,
+    [string]$role
+)
+
+$ErrorActionPreference = 'Stop'
+
+Import-Module SPE
+Write-Host ("Connecting to {0}" -f $instanceUrl)
+
+$session = New-ScriptSession -Username $adminUsername -Password $adminPassword -ConnectionUri $("https://" + $instanceUrl)
+
+Invoke-RemoteScript -Session $session -ScriptBlock { 
+    Add-RoleMember -Identity $using:role -Members $using:username
+}
--- scripts/SPE/set-hostname.ps1
+++ scripts/SPE/set-hostname.ps1
@@ -1,13 +1,15 @@
-param([string]$instanceUrl)
+param(
+    [string]$instanceUrl = "habitathome.dev.local",
+    [string]$hostname,
+    [string]$adminUsername,
+    [string]$adminPassword)
 $ErrorActionPreference = 'Stop'
 
-$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
-
-# This is an example PowerShell script that will remotely execute a Unicorn sync using the new CHAP authentication system.
-
 Import-Module SPE
-Write-Host ("Connecting to {0}"-f $instanceUrl)
+Write-Host ("Connecting to {0}" -f $instanceUrl)
 
-$session = New-ScriptSession -Username "admin" -Password "b" -ConnectionUri $("https://" + $instanceUrl)
+$session = New-ScriptSession -Username "$adminUsername" -Password "$adminPassword" -ConnectionUri $("https://" + $instanceUrl)
 
-Invoke-RemoteScript -Session $session -ScriptBlock { Get-Item -Path "master:/content/Habitat Sites/Habitat Home/Settings/Site Grouping/Habitat Home" | ForEach-Object {$_.HostName = "$instanceUrl"}}
+Invoke-RemoteScript -Session $session -ScriptBlock { 
+    (Get-Item -Path "master:/content/Habitat Sites/Habitat Home/Settings/Site Grouping/Habitat Home").HostName = ("{0}" -f $using:hostname)
+}
