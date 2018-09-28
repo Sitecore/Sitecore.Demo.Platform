e9cdb998b9c130fc6f5cd3c4a76d5a1280fb866b
Scott Mulligan <scm@sitecore.net>
2018-09-27 22:00:50 -0400

    Big Update! - Split up the Feature.CRM project into two projects: - Feature.CrmDynamics - Feature.CrmSalesforce
    Was forced to split this project up due to the serialization config files for Dynamics and Salesforce. Couldn't have the 2 together as they were attempting to serialize to the same folder.

---
+++ src/Feature/CrmSalesforce/code/SubmitActions/Models/SyncToSalesforceCrmActionData.cs
@@ -0,0 +1,13 @@
+﻿using System;           
+
+namespace Sitecore.HabitatHome.Feature.CRM.SubmitActions.Models
+{
+    public class SyncToSalesforceCrmActionData
+    {
+        public Guid ReferenceId
+        {
+            get;
+            set;
+        }
+    }
+}
