ca1e08f43088648f4c36fa9b626aaabbb69a0333
Aron Boyette <aboyette@hhogdev.com>
2018-10-22 10:56:19 -0400

    Initial creation of Selenium Test and HabitatHomeTest.

---
+++ src/Project/HabitatHome/tests/HabitatHomeTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Sitecore.HabitatHome.Website.Test
+{
+    [TestFixture]
+    public class HabitatHomeTest : SeleniumTest
+    {
+        [Test]
+        public void TestNewUser()
+        {
+            GoTo($"{Host}/landing-pages/bing-smart-home-design");
+            Click("How to Design Your Smart Home");
+            Wait("button.btn-info.sidebar-closed");
+            Click("button.btn-info.sidebar-closed");
+            Click("Onsite Behavior");
+
+            var elements = GetElements("#onsiteBehaviorPanel div.media div.tab-content");
+            Assert.IsTrue(!elements.Any(e => e.Text.Contains("You have triggered no goals so far")));
+        }
+
+    }
+}
