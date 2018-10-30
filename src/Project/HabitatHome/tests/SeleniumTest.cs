93ff3e5383ef55ad8e29cdc7f2f6fc24ebdd430f
Aron Boyette <aboyette@hhogdev.com>
2018-10-30 08:59:49 -0400

    Completed Selenium and Backstop integration.

--- src/Project/HabitatHome/tests/SeleniumTest.cs
+++ src/Project/HabitatHome/tests/SeleniumTest.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -12,33 +13,18 @@ namespace Sitecore.HabitatHome.Website.Test
 {
     public class SeleniumTest
     {
+        private string bitmapsPath;
+        private static string codeBasePath;
         private IWebDriver driver;
-        protected IWebDriver Driver
-        {
-            get
-            {
-                if (driver != null)
-                    return driver;
-
-                driver = new ChromeDriver();
-                return driver;
-            }
-        }
-
-        public string Host { get; set; } = "http://habitathome.dev.local";
 
 
 
-        [SetUp]
-        public void Init()
+        protected void ApplyConfig()
         {
             try
             {
-                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-                UriBuilder uri = new UriBuilder(codeBase);
-                string path = Uri.UnescapeDataString(uri.Path);
-                path = Path.GetDirectoryName(path);
-                string json = File.ReadAllText(Path.Combine(path, GetType().Name + ".json"));
+                string jsonFilename = Path.Combine(CodeBasePath, GetType().Name + ".json");
+                string json = File.ReadAllText(jsonFilename);
                 JsonConvert.PopulateObject(json, this);
             }
             catch (Exception ex)
@@ -51,10 +37,58 @@ namespace Sitecore.HabitatHome.Website.Test
 
 
 
+        public string BackstopPath { get; set; } = FindBackstopPath();
+
+
+
+        public string BitmapsPath
+        {
+            get
+            {
+                return bitmapsPath ??
+                    (bitmapsPath = Path.Combine(BackstopPath, "backstop_data", "bitmaps_test",
+                        DateTime.Now.ToString("yyyyMMdd-HHmmss")));
+            }
+        }
+
+
+
+        protected static string CodeBasePath
+        {
+            get
+            {
+                if (codeBasePath != null)
+                    return codeBasePath;
+
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                codeBasePath = Path.GetDirectoryName(path);
+                return CodeBasePath;
+            }
+        }
+
+
+
         protected void Click(string descriptor)
         {
             Console.WriteLine($"clicking \"{descriptor}\"");
             GetElement(descriptor).Click();
+        }
+
+
+
+        protected IWebDriver Driver
+        {
+            get
+            {
+                if (driver != null)
+                    return driver;
+
+                driver = new ChromeDriver();
+                driver.Manage().Window.Size = new Size(1200, 800);
+                return driver;
+            }
         }
 
 
@@ -70,6 +104,29 @@ namespace Sitecore.HabitatHome.Website.Test
             {
                 return false;
             }
+        }
+
+
+
+        protected static string FindBackstopPath()
+        {
+            string path = CodeBasePath;
+            string backstopPath = Path.Combine(path, "backstop");
+            while (!Directory.Exists(Path.Combine(backstopPath, "backstop_data")))
+            {
+                DirectoryInfo dir = Directory.GetParent(path);
+
+                // If dir is null, then we've climbed the directory tree
+                // as far as we can go.  The backstop directory either
+                // doesn't exist or it is somewhere out of reach.  Just
+                // return the CodeBasePath.
+                if (dir == null)
+                    return CodeBasePath;
+
+                path = dir.FullName;
+                backstopPath = Path.Combine(path, "backstop");
+            }
+            return backstopPath;
         }
 
 
@@ -100,6 +157,42 @@ namespace Sitecore.HabitatHome.Website.Test
         {
             Console.WriteLine($"going to {url}");
             Driver.Navigate().GoToUrl(url);
+        }
+
+
+
+        public string Host { get; set; } = "http://habitathome.dev.local";
+
+
+
+        public void TakeScreenshot(string name)
+        {
+            var its = driver as ITakesScreenshot;
+            Screenshot s = its.GetScreenshot();
+            Directory.CreateDirectory(BitmapsPath);
+            name = Path.Combine(BitmapsPath,
+                $"{GetType().Name}_{TestContext.CurrentContext.Test.Name}_{name}.png");
+            s.SaveAsFile(name, ScreenshotImageFormat.Png);
+        }
+
+
+
+        [TearDown]
+        public void TestCleanup()
+        {
+            if (driver != null)
+            {
+                TakeScreenshot("final");
+                driver.Dispose();
+            }
+        }
+
+
+
+        [SetUp]
+        public void TestInit()
+        {
+            ApplyConfig();
         }
 
 
