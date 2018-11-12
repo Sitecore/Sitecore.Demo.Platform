164db18e2fd6e9c8aa7e363ae108688562086623
Nikola Gotsev <nsgocev@gmail.com>
2018-11-12 14:10:07 +0200

    Smart Publish Post Step

---
+++ src/Project/Common/code/Utilities/Installation/SmartPublishPostStep.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Specialized;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Install.Framework;
+using Sitecore.Publishing;
+
+namespace Sitecore.HabitatHome.Common.Website.Utilities.Installation
+{
+    public class SmartPublishPostStep : IPostStep
+    {
+        private static readonly Database _masterDatabase = Factory.GetDatabase("master");
+        private static readonly Database _webDatabase = Factory.GetDatabase("web");
+
+        public void Run(ITaskOutput output, NameValueCollection metaData)
+        {
+            try
+            {
+                Assert.IsNotNull(_masterDatabase, "Master Database can't be null");
+                Assert.IsNotNull(_webDatabase, "Web Database can't be null.");
+
+                using (new SecurityModel.SecurityDisabler())
+                {
+                    Item sitecoreRootItem = _masterDatabase.GetItem("/sitecore");
+
+                    if (sitecoreRootItem == null)
+                    {
+                        throw new NullReferenceException("The '/sitecore' item does not exist in Sitecore.");
+                    }
+
+                    Language[] languages = _masterDatabase.Languages;
+
+                    foreach (Language language in languages)
+                    {
+                        PublishOptions publishOptions = new PublishOptions(
+                            _masterDatabase,
+                            _webDatabase,
+                            PublishMode.Smart,
+                            language,
+                            DateTime.Now)
+                        {
+                            Deep = true
+                        };
+
+
+                        Publisher publisher = new Publisher(publishOptions);
+                        publisher.Options.RootItem = sitecoreRootItem;
+                        publisher.PublishAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex, this);
+            }
+        }
+    }
+}
