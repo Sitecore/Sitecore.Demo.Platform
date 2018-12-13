1f55936e40c436bef4718e63d0383fc39bdc509d
Scott Mulligan <scm@sitecore.net>
2018-12-13 12:35:48 -0500

    Added custom Unicorn predicate - It will only serialize unicorn items in this predicate if it can find the item's template

---
+++ src/Foundation/Serialization/code/Unicorn/Predicates/EnsureTemplatePredicate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Unicorn.Predicates;
+using System.Xml;
+using Rainbow.Model;
+using Unicorn.Configuration;
+using Unicorn.Data.DataProvider;
+
+namespace Sitecore.HabitatHome.Foundation.Serialization.Unicorn.Predicates
+{
+    public class EnsureTemplatePredicate : SerializationPresetPredicate
+    {
+        public EnsureTemplatePredicate(XmlNode configNode, IUnicornDataProviderConfiguration dataProviderConfiguration, IConfiguration configuration) : base(configNode, dataProviderConfiguration, configuration)
+        {
+        }
+
+        protected new PredicateResult Includes(PresetTreeRoot entry, IItemData itemData)
+        {
+            // check for db match
+            if (!itemData.DatabaseName.Equals(entry.DatabaseName, StringComparison.OrdinalIgnoreCase)) return new PredicateResult(false);
+
+            // check for path match
+            var unescapedPath = entry.Path.Replace(@"\*", "*");
+            if (!itemData.Path.StartsWith(unescapedPath + "/", StringComparison.OrdinalIgnoreCase) && !itemData.Path.Equals(unescapedPath, StringComparison.OrdinalIgnoreCase))
+            {
+                return new PredicateResult(false);
+            }
+
+            // check that the item's template exists
+            var templateId = itemData.TemplateId;
+            var templateItem = Sitecore.Configuration.Factory.GetDatabase(itemData.DatabaseName).GetItem(templateId.ToString());
+            if (templateItem == null)
+            {
+                return new PredicateResult(false);
+            }
+
+            // check excludes
+            return ExcludeMatches(entry, itemData);
+        }
+    }
+}
