a98c8517c22acacb375d55d1275aeb528f107b25
Robbert Hock <robbert.hock@sitecore.com>
2019-04-24 14:33:53 +0100

    PIB 2382 Video Component - Configured Video & video folder templates, added data example content

---
+++ src/Feature/Media/code/Controllers/MediaController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Sitecore.HabitatHome.Feature.Media.Controllers
+{
+    public class MediaController : Controller
+    {
+        public ViewResult Video()
+        {
+            return new ViewResult();
+        }
+    }
+}
