3b54cb3712a9f682f3876b85e666473a7e2cd6e7
Robbert Hock <robbert.hock@sitecore.com>
2019-04-29 22:28:29 +0200

    PIB 2385 - Added news and news-overview pages + implemented basic bootstrap look and feel for the NewsOverview

---
+++ src/Feature/News/code/Controllers/NewsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Sitecore.HabitatHome.Feature.News.Controllers
+{
+    public class NewsController : Controller
+    {
+        public ViewResult NewsOverview()
+        {
+            return View("~/Areas/News/Views/NewsOverview.cshtml");
+        }
+    }
+}
