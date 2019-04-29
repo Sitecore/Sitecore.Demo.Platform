d3a51a645cbadddd6ebd7e245d4a2bd9ca93fe41
Robbert Hock <robbert.hock@sitecore.com>
2019-04-29 20:57:42 +0200

    PIB 2385- News - Added serialization config + area registration

---
+++ src/Feature/News/code/Areas/News/NewsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Sitecore.HabitatHome.Feature.News.Areas.News
+{
+    public class NewsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "News";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "News_default",
+                "News/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
