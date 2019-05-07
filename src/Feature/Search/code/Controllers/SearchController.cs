32d2eb96e01aa62538cc9e2d657c4be35a1114b7
Robbert Hock <robbert.hock@sitecore.com>
2019-05-07 11:32:26 +0200

    PBI 2378 Search - Moved Search form to it's own component, added searchcontroller

---
+++ src/Feature/Search/code/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Sitecore.HabitatHome.Feature.Search.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ViewResult Search()
+        {
+            return View("~/Areas/Search/Views/Search.cshtml");
+        }
+    }
+}
