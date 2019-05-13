def1c67ed96aba5ce41ce319d4bc2a3773123278
Elena Mosoff <emosoff@hhogdev.com>
2019-05-13 11:41:32 -0400

    cleaning up projects, which have been converted to packages, from solution

--- src/Feature/Accounts/code/Services/INotificationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Sitecore.HabitatHome.Feature.Accounts.Services
-{
-    public interface INotificationService
-    {
-        void SendPassword(string email, string newPassword);
-    }
-}
