9c17f5928f249e639002c5f77a08a5ce7511d8fd
Jean-François Larente <jfl@sitecore.net>
2019-10-10 15:56:11 -0400

    Added exlusion for .gitkeep in clean data script

--- CleanDockerData.ps1
+++ CleanDockerData.ps1
@@ -5,7 +5,7 @@ Param(
 
 foreach ($folder in $Folders)
 {
-    Get-ChildItem -Path $folder -Recurse | Remove-Item -force -recurse
+    Get-ChildItem -Path $folder -Recurse | Remove-Item -force -recurse -Exclude .gitkeep
 }
 
 Get-ChildItem .\data -Recurse | Remove-Item -Force -Recurse -Exclude .gitkeep
