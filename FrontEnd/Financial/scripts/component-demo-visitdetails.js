4c6737bfb7ce8158bd356e61f0e228fcdeb76fe2
Minhaz Topaz <mint@sitecore.net>
2021-01-26 16:27:48 +0000

    Merged PR 3192: added page refresh if new visitor is clicked on the demo sidebar
    added page refresh if new visitor is clicked on the demo sidebar
    line #11 is the only change, rest is formatting...
    
    Video showing the work
    https://www.loom.com/share/67dd605ae0b84022ad9e6b89a42f0653
    my mic was off but I was talking about how I just added the line to refresh page on the new visitor action for all four themes :)
    
    Related work items: #5785

--- FrontEnd/Financial/scripts/component-demo-visitdetails.js
+++ FrontEnd/Financial/scripts/component-demo-visitdetails.js
@@ -1,50 +1,50 @@
-﻿(function ($) {
-    var refreshExperincePanel = function () {
+﻿(function($) {
+    var refreshExperincePanel = function(reloadPage) {
         var panel = $("#experiencedata");
-        $.ajax(
-            {
-                url: "/api/Demo/ExperienceDataContent",
-                method: "get",
-                cache: false,
-                success: function (data) {
-                    panel.replaceWith(data);
-                    $("#sidebar").find('.panel-collapse').hide();
+        $.ajax({
+            url: "/api/Demo/ExperienceDataContent",
+            method: "get",
+            cache: false,
+            success: function(data) {
+                panel.replaceWith(data);
+                $("#sidebar").find('.panel-collapse').hide();
+                if (reloadPage) {
+                    window.location.href = window.location.href;
                 }
-            });
+            }
+        });
     };
-    $(function () {
+    $(function() {
         $(".refresh-sidebar")
-            .click(function () {
+            .click(function() {
                 $(this).fadeOut();
-                refreshExperincePanel();
+                refreshExperincePanel(false);
                 $(this).fadeIn();
             });
         $(".end-visit")
-            .click(function () {
+            .click(function() {
                 $(this).fadeOut();
-                $.ajax(
-                    {
-                        url: "/api/Demo/EndVisit",
-                        method: "get",
-                        cache: false,
-                        success: function () {
-                            refreshExperincePanel();
-                        }
-                    });
+                $.ajax({
+                    url: "/api/Demo/EndVisit",
+                    method: "get",
+                    cache: false,
+                    success: function() {
+                        refreshExperincePanel(false);
+                    }
+                });
                 $(this).fadeIn();
             });
         $(".new-contact")
-            .click(function () {
+            .click(function() {
                 $(this).fadeOut();
-                $.ajax(
-                    {
-                        url: "/api/Demo/NewContact",
-                        method: "get",
-                        cache: false,
-                        success: function () {
-                            refreshExperincePanel();
-                        }
-                    });
+                $.ajax({
+                    url: "/api/Demo/NewContact",
+                    method: "get",
+                    cache: false,
+                    success: function() {
+                        refreshExperincePanel(true);
+                    }
+                });
                 $(this).fadeIn();
             });
     });
