95163f38c448d065fa8410f7daa136657889a634
Jean-François Larente <jfl@sitecore.net>
2021-02-19 11:52:46 -0500

    Removed test string

--- docker/images/windows/demo-solution/scripts/Add-InternalNugetFeed.ps1
+++ docker/images/windows/demo-solution/scripts/Add-InternalNugetFeed.ps1
@@ -1,5 +1,4 @@
 
-Write-Host $PWD
 if (-not ([string]::IsNullOrEmpty($env:INTERNAL_NUGET_SOURCE) -and [string]::IsNullOrEmpty($env:INTERNAL_NUGET_SOURCE_USERNAME) -and [string]::IsNullOrEmpty($env:INTERNAL_NUGET_SOURCE_PASSWORD))) {
     Write-Host "Adding internal nuget source"
     dotnet nuget add source "$($env:INTERNAL_NUGET_SOURCE)" --name "sc-internal-package-feed" --username "$($env:INTERNAL_NUGET_SOURCE_USERNAME)" --password "$($env:INTERNAL_NUGET_SOURCE_PASSWORD)" --configfile "nuget.config" --store-password-in-clear-text
